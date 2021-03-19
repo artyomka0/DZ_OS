ab4a1e1c39347eac09f5f12512b13c67b2171b4a
artyomka0 <qwertyartyom772@gmail.com>
2021-03-19 19:37:52 +0300

    create driver & json work

--- DZ_OS/MainWindow.xaml.cs
+++ DZ_OS/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace DZ_OS
 {
@@ -23,6 +25,55 @@ namespace DZ_OS
     public MainWindow()
     {
       InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      ChromeOptions chromeOptions = new ChromeOptions();
+      chromeOptions.AddArgument(@"user-data-dir=C:\Users\Titanicus\AppData\Local\Google\Chrome\User Data");
+      ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
+      chromeDriver.Navigate().GoToUrl("https://vk.com/feed");
+      List<IWebElement> webElements = chromeDriver.FindElementsById("feed_rows").ToList();
+      IWebElement webElementParent = null;
+      foreach (var item in webElements)
+      {
+        if (!item.Displayed)
+          continue;
+        webElementParent = item;
+        break;
+      }
+      if (webElementParent == null)
+        return;
+      webElements = webElementParent.FindElements(By.TagName("div")).ToList();
+      foreach (var item in webElements)
+      {
+        if (!item.Displayed)
+          continue;
+        if (item.GetAttribute("class") == null)
+          continue;
+        if (!item.GetAttribute("class").Trim().Equals("feed_row"))
+          continue;
+        string NewsText = item.Text;
+        List<IWebElement> webElementsChild = item.FindElements(By.ClassName("author")).ToList();
+        foreach (var itemChild in webElementsChild)
+        {
+          if (!itemChild.Displayed)
+            continue;
+          string author = itemChild.Text;
+          // string id = itemChild.GetAttribute("data-post-id").Trim();
+        }
+        webElementsChild = item.FindElements(By.TagName("span")).ToList();
+        foreach (var itemChild in webElementsChild)
+        {
+          if (!itemChild.Displayed)
+            continue;
+          if (itemChild.GetAttribute("time") == null)
+            continue;
+          DateTime dateTime = new DateTime(1970, 1, 1).AddSeconds(double.Parse(itemChild.GetAttribute("time"))).ToLocalTime();
+
+        }
+
+      }
     }
   }
 }
---
+++ DZ_OS/WorkWithJSON.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace DZ_OS
+{
+  
+    [DataContract]
+    public class News
+    {
+      [DataMember]
+      public string Id;
+      [DataMember]
+      public string Text;
+    }
+
+    class WorkWithJSON
+    {
+      public static void SetNewsInJSON(News[] news)
+      {
+        DataContractSerializer dataContractJsonSerializer = new DataContractSerializer(typeof(News[]));
+        using (FileStream file = new FileStream("студент1.json", FileMode.OpenOrCreate))
+        {
+          dataContractJsonSerializer.WriteObject(file, news);
+        }
+      }
+     /*
+      public static Student GetDataFromJSON()
+      {
+        if (!File.Exists("студентик.json"))
+          return new Student();
+        DataContractJsonSerializer dataContractJsonSerializer = new DataContractJsonSerializer(typeof(Student));
+        using (FileStream file = new FileStream("студентик.json", FileMode.Open))
+        {
+          return (Student)dataContractJsonSerializer.ReadObject(file);
+        }
+      }
+     */
+    }
+  }
+}
